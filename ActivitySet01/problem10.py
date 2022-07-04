3dba34f4983a44cead4197449ff7c681ab49e009
mounika bonagiri <94587952+BONAGIRIMOUNIKA@users.noreply.github.com>
2022-07-04 04:18:32 +0000

    Done

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,21 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+d = dict()
+filename = "mbox-short.txt"
+def getmail(filename):
+    with open(filename) as f:
+        for lines in f:
+            word = lines.rstrip().split()
+            if 'From' in word and'From:' not in word:
+                email = word[1]
+                d[email] = d.get(email, 0) + 1
+
+    cnt =0
+    mail = None
+
+    for email,count in d.items():
+        if count>cnt:
+            cnt,mail = count, email
+
+    return mail, cnt
